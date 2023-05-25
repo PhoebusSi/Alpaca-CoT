0b84e927f2e43abb95ef0a40a568c3c0cb5aae82
dkqkxx <dkqkxx@gmail.com>
2023-05-25 21:56:34 +0800

    rm pytorch_model.bin

--- utils/save.py
+++ utils/save.py
@@ -6,4 +6,8 @@ class SavePeftModelCallback(TrainerCallback):
     def on_save(self,args: TrainingArguments,state: TrainerState,control: TrainerControl,**kwargs,):
         checkpoint_folder = os.path.join(args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}")
         kwargs["model"].save_pretrained(checkpoint_folder)
+        
+        pytorch_model_path = os.path.join(checkpoint_folder, "pytorch_model.bin")
+        if os.path.exists(pytorch_model_path):
+            os.remove(pytorch_model_path)
         return control
